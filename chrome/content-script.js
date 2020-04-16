92144057f61e6d30e3f9bb93c8dc4a7e0a0c91d8
ritsource <ritwiksahaofficial@gmail.com>
2020-04-17 01:24:43 +0530

    Only showing IDs of visible links

--- chrome/content-script.js
+++ chrome/content-script.js
@@ -5,44 +5,55 @@ const EscKeyCode = 101;
 
 let i = 0;
 
-const make_id_element = ({ left, top }) => {
-    const circle = document.createElement("div");
+const BORDER_COLOR = "rgba(33, 33, 33, 0.5)";
+const BACKGROUND_COLOR = "rgba(255, 255, 255, 0.5)";
+const TEXT_COLOR = "rgba(33, 33, 33, 1)";
 
-    circle.style.backgroundColor = "white";
-    circle.style.color = "#333333";
-    circle.style.border = "1px solid #333333";
-    circle.style.borderRadius = "50%";
-    circle.style.fontSize = "12px";
-    // circle.style.display = "inline-block";
-    circle.style.position = "absolute";
-    circle.style.position = "absolute";
-    // circle.style.top = top;
-    // circle.style.left = left;
-    circle.style.top = `${top}px`;
-    circle.style.left = `${left}px`;
-    circle.style.zIndex = 99;
+const make_id_element = ({ left, top }) => {
+    const c = document.createElement("div");
+
+    c.style.backgroundColor = `${BACKGROUND_COLOR}`;
+    c.style.border = `1px solid ${BORDER_COLOR}`;
+    c.style.borderRadius = "50%";
+    c.style.position = "absolute";
+    c.style.position = "absolute";
+    c.style.top = `${top}px`;
+    c.style.left = `${left}px`;
+    c.style.zIndex = 99;
 
     // console.log({ left, top });
-    
-    circle.appendChild(document.createTextNode(i));
+
+    const p = document.createElement("p");
+    p.style.color = `${TEXT_COLOR}`;
+    p.style.fontSize = "10px";
+    p.style.padding = "2px 2px 2px 2px";
+    p.style.margin = "0px";
+    // p.style.opacity = 0.2;
+
+    p.appendChild(document.createTextNode(i));
+    c.appendChild(p);
     // circle.insertBefore(document.createTextNode(i));
     i += 1;
 
-    return circle;
+    return c;
 };
 
 const show_numbers = () => {
+    const body = document.querySelector("body");
+
     document.querySelectorAll("a").forEach((el, i) => {
-        el.style.textDecoration = "underline";
+        const not_visible = find_propigated_style(el, "visibility", "hidden")
+            || find_propigated_style(el, "display", "none");
         
+        if (not_visible) {
+            return
+        }
+
         const { left, top } = el.getBoundingClientRect();
-        
+
         const circle = make_id_element({ left, top });
 
-        // el.appendChild(circle);
-        document
-            .querySelector("body")
-            .appendChild(circle);
+        body.appendChild(circle);
     })
 }
 
@@ -56,3 +67,15 @@ document.onkeypress = (e) => {
         console.log(e);
     }
 };
+
+const find_propigated_style = (el, key, value) => {
+    while (el.parentNode) {
+        if (el.style[key] === value) {
+            return true;
+        }
+
+        el = el.parentNode;
+    }
+
+    return false;
+}
